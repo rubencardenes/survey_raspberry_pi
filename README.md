4f3234af7f2e72b3d2a03d7f88ca1c905a7a97e9
Ruben Cardenes <ruben.cardenes@hensoldt.net>
2020-04-08 14:26:51 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# Video surveillance using Raspberry PI or USB Camera and Flask
+
+## Author 
+Ruben Cardenes, PhD
+
+## Description
+
+This project starts a web server application with Flask that streams video from a camera. 
+ It uses motion detection, shows the detected moving objects with red squares, and stores the frames with motion on disk with minimum time separation of 2 seconds. 
+The web application allows to see the streaming video a summary of the moving pictures and creates a video summary of the current day.
+ 
+ ## Project features 
+
+- [x] Raspberry PI camera support
+- [x] USB Camera support 
+- [x] Multi-object motion detection for picture saving 
+- [x] Web streaming 
+- [x] Direct streaming scripts  
+- [x] Picture gallery summary
+- [x] Video summary creation from saved images 
+- [ ] People detection (for a future version)
+
+![image](example.jpg)
+
+## Requirements
+
+Python 3.6 
+
+```
+Flask==1.1.2
+imutils==0.5.3
+numpy==1.18.2
+opencv-python>=3.4.6.27
+tqdm==4.45.0
+```
+
+## Instructions 
+
+1. Download the code:
+```
+git clone https://github.com/rubencardenes/survey_raspberry_pi.git
+```
+
+2. Install python requirements (preferably in a virtual environment) 
+```
+pip3 install -r requirements.txt 
+```
+
+3. Folder structure 
+
+Make sure that you have a folder called "static" at the same level as the scripts. 
+Inside static, you also need to have the folders "images" and "video_summary"
+ 
+4. Start the script (by default on localhost in port 8887)
+```
+python3 start_flask.py &
+```
+The & at the end of the is not mandatory, but it makes the script to run on the background. 
+
+5. Connect to the server from a browser:
+```
+http://localhost:8887
+```
+
+Notes: the main script usage is the following  
+
+```
+usage: start_flask.py [-h] [--port PORT] [--host HOST] [-p CAMERA_TYPE]
+
+optional arguments:
+  -h, --help      show this help message and exit
+  --port PORT     socket port
+  --host HOST     destination host name or ip
+  -p CAMERA_TYPE  use piCamera
+```
+## Extra
+
+There are two extra scripts to do just video streaming:
+
+- video_stream_receive.py
+- video_stream_send.py  
+
+Those ones do not use Flask, but send video signal directly to another PC. Can be useful for some applications. 
+Important to know is that video_stream_receive.py acts as server and should be started first to start listening to incoming connections. 
+
+## License 
+This is opened with a MIT License, so use it as you want.    
+
+## Acknowledgements
+
+Some code for Flask integration and to do motion detection are taken from the great PyImageSearch blog by Adrian Rosebrock. 
+Here some extra code  
+
---
+++ start_flask_RPi.py
@@ -0,0 +1,189 @@
+# ###################################################################
+# Ruben Cardenes -- Apr 2020
+#
+# File:        start_flask_RPi.py
+# Description: This scripts starts a web app in the Raspberry pi for video surveillance
+#              Users can connect to the video streaming using a web browser.
+#
+# ###################################################################
+
+import time
+import numpy as np
+import os
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+import threading
+from argparse import ArgumentParser
+from MultiObjectMotionDetection import MultiObjectMotionDetector
+from flask import Response
+from flask import Flask
+from flask import render_template
+from create_video import make_video
+
+
+class MemorySharing():
+
+    def __init__(self):
+        self.frame = None
+
+    def generate_frames(self):
+        while True:
+            with lock:
+                if self.frame is None:
+                    continue
+                (flag, encodedImage) = cv2.imencode(".jpg", self.frame)
+
+                # ensure the frame was successfully encoded
+                if not flag:
+                    continue
+
+                # Add header to frame in byte format
+                bytes_to_send = (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                                 bytearray(encodedImage) + b'\r\n')
+
+            yield bytes_to_send
+
+
+# Video Sending Thread
+class VideoSendThread(Thread):
+    # A class to send video frames using threads
+    # This class inherits from Thread, which means that will run on a separate Thread
+    # whenever called, it starts the run method
+
+    def __init__(self, container, camera_resolution=(640, 480)):
+        Thread.__init__(self)
+        # create socket and bind host
+        self.camera_resolution = camera_resolution
+        self.stopped = False
+        self.md = MultiObjectMotionDetector()
+        self.output_name = ""
+        self.save_time = 0
+        self.container = container
+        self.vid = cv2.VideoCapture(0)
+
+    def save_frame(self, frame):
+        date_time = time.strftime("%m_%d_%Y-%H:%M:%S")
+        self.output_name = os.path.join("./static/images", date_time + ".jpg")
+        self.save_time = time.time()
+        cv2.imwrite(self.output_name, frame)
+        cv2.putText(frame, "Saving frame", (0, 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
+
+    def run(self):
+        try:
+
+            with PiCamera() as camera:
+                camera.resolution = self.camera_resolution  # pi camera resolution
+                camera.framerate = 10  # 10 frames/sec
+                time.sleep(2)  # give 2 secs for camera to initialize
+                rawCapture = PiRGBArray(camera, size=self.camera_resolution)
+                stream = camera.capture_continuous(rawCapture,
+                                                   format="bgr", use_video_port=True)
+
+                # send jpeg format video stream
+                for f in stream:
+                    frame = f.array
+                    rawCapture.truncate(0)
+
+                    # MOTION DETECTION
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    gray_blur = cv2.GaussianBlur(gray, (7, 7), 0)
+                    thresh = np.zeros(frame.shape)
+                    self.md.update(gray_blur)
+                    thresh, md_boxes = self.md.detect(gray_blur)
+                    if md_boxes is not None:
+                        total_area = 0
+                        for b in md_boxes:
+                            cv2.rectangle(frame, (b[0], b[1]), (b[2], b[3]),
+                                          (0, 0, 255), 1)
+                            total_area += (b[2] - b[0])*(b[3] - b[1])
+                        if total_area > 500:
+                            # print("boxes: ", md_boxes)
+                            if time.time() - self.save_time > 3:
+                                self.save_frame(frame)
+
+                    with lock:
+                        # print("sending frame")
+                        self.container.frame = frame
+
+        finally:
+            self.stopped = True
+
+
+app = Flask(__name__)
+lock = threading.Lock()
+container = MemorySharing()
+
+
+@app.route("/")
+def index():
+    # return the rendered template
+    return render_template("index.html")
+
+
+@app.route("/summary")
+def summary():
+    imgs = ["images/" + file for file in os.listdir('static/images')]
+    imgs.sort()
+    # return the rendered template
+    return render_template("summary.html", imgs=imgs)
+
+
+@app.route("/make_summary")
+def make_summary():
+    day = time.strftime("%m_%d_%Y")
+    imgs = ["static/images/" + file for file in os.listdir('static/images') if day in file]
+    imgs.sort()
+    make_video(imgs, f"static/video_summary/{day}.mp4")
+    # return the rendered template
+    return render_template("video_summary.html")
+
+
+@app.route("/video_summary")
+def video_summary():
+    vids = ["video_summary/" + file for file in os.listdir('static/video_summary')]
+    vids.sort()
+    # return the rendered template
+    return render_template("video_summary.html", vids=vids)
+
+
+@app.route("/video_feed")
+def video_feed():
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(container.generate_frames(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument('--port', type=int,
+                        dest='port',
+                        default=8887,
+                        help='socket port',
+                        required=False)
+    parser.add_argument('--host', type=str,
+                        dest='host',
+                        default='0.0.0.0',
+                        help='destination host name or ip',
+                        required=False)
+    args = vars(parser.parse_args())
+
+    host = args['host']
+    port = args['port']
+
+    threads = []
+
+    newthread = VideoSendThread(container)
+    newthread.start()
+    threads.append(newthread)
+
+    # initialize a flask object
+
+    app.run(host=args["host"], port=args["port"], debug=True,
+            threaded=True, use_reloader=False)
+
+    for t in threads:
+        t.join()
